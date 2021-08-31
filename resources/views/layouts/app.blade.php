bd22e3a700eb87ce8d32310936ff67dea22abb89
d-nadiradze <d_nadiradze4@cu.edu.ge>
2021-08-31 12:31:42 +0400

    private chat

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
         <link rel="stylesheet" href="{{ asset('css/scroll.css') }}">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.1.2/socket.io.js"></script>
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
